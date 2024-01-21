3a13d817e77a00e83f45e8b1486a89de76c354b1
KarthikShetty27 <karthikshetty270902@gmail.com>
2024-01-21 23:20:08 +0530

    Changes-2.1

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -21,81 +21,70 @@ function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Age:
-        <input
-          type="text"
-          value={age}
-          onChange={(event) => setAge(event.target.value)}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Socioeconomic Background:
-        <select
-          value={seb}
-          onChange={(event) => setSeb(event.target.value)}
-          required
-        >
-          <option value="">--Please select an option--</option>
-          <option value="Open">Open</option>
-          <option value="SQ - State Quota">SQ - State Quota</option>
-          <option value="SC - Scheduled Castes">SC - Scheduled Castes</option>
-          <option value="ST - Scheduled Tribes">ST - Scheduled Tribes</option>
-          <option value="EWS - Economically Weaker Sections">
-            EWS - Economically Weaker Sections
-          </option>
-          <option value="OBC - Other Backward Classes">
-            OBC - Other Backward Classes
-          </option>
-        </select>
-      </label>
-      <br />
-      <label>
-        SSC Marks:
-        <input
-          type="number"
-          step="0.01"
-          value={sscMarks}
-          onChange={(event) => setSscMarks(event.target.value)}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        HSC Marks:
-        <input
-          type="number"
-          step="0.01"
-          value={hscMarks}
-          onChange={(event) => setHscMarks(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        MHTCET Marks:
-        <input
-          type="number"
-          step="0.01"
-          value={mhtcetMarks}
-          onChange={(event) => setMhtcetMarks(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        JEE Mains Marks:
-        <input
-          type="number"
-          step="0.01"
-          value={jeeMainsMarks}
-          onChange={(event) => setJeeMainsMarks(event.target.value)}
-        />
-      </label>
-      <br />
-      <input type="submit" value="Submit" />
-    </form>
+    <div id="finput">
+      <h2> Form Details </h2>
+      <div className='form-data'>
+        <form onSubmit={handleSubmit}>
+        {/* Age */}
+          <div className="form-group row">
+            <label for="Age"  className="col-sm-4 col-form-label">Age: </label>
+            <div className="col-sm-8">
+              <input className="form-control" id="Age" type="text" placeholder="85.56" value={age} onChange={(event) => setAge(event.target.value)} required />
+            </div>
+          </div>
+          <br />
+        {/* Socioeconomic Background */}
+          <div  className="form-group row">
+            <label for="SEB"  className="col-sm-5 col-form-label">Socioeconomic Background: </label>
+            <div  className="col-sm-7">
+              <select id="inputState"  className="form-control" value={seb} onChange={(event) => setSeb(event.target.value)} required >
+              <option value="">--Please select an option--</option>
+              <option value="Open">Open</option>
+              <option value="SQ - State Quota">SQ - State Quota</option>
+              <option value="SC - Scheduled Castes">SC - Scheduled Castes</option>
+              <option value="ST - Scheduled Tribes">ST - Scheduled Tribes</option>
+              <option value="EWS - Economically Weaker Sections"> EWS - Economically Weaker Sections </option>
+              <option value="OBC - Other Backward Classes"> OBC - Other Backward Classes </option>
+              </select>
+            </div>
+          </div>
+          <br />
+        {/* SSC Marks */}
+          <div  className="form-group row">
+            <label for="SSC"  className="col-sm-4 col-form-label">SSC Marks: </label>
+            <div  className="col-sm-8">
+              <input type="ssc-marks" step="0.01" className="form-control" id="SSC" placeholder="85.56" value={sscMarks} onChange={(event) => setSscMarks(event.target.value)} required />
+            </div>
+          </div>
+          <br />
+        {/* HSC Marks */}
+          <div  className="form-group row">
+            <label for="HSC"  className="col-sm-4 col-form-label">HSC Marks: </label>
+            <div  className="col-sm-8">
+                <input type="hsc-marks" step="0.01" className="form-control" id="HSC" placeholder="88.64"  value={hscMarks} onChange={(event) => setHscMarks(event.target.value)} required />
+            </div>
+          </div>
+          <br />
+         {/* MHTCET Marks */}
+         <div  className="form-group row">
+            <label for="MHTCET"  className="col-sm-4 col-form-label">MHTCET Marks: </label>
+            <div  className="col-sm-8">
+                <input type="mhtcet-marks" step="0.01" className="form-control" id="MHTCET" placeholder="90.85" value={mhtcetMarks} onChange={(event) => setMhtcetMarks(event.target.value)} required />
+            </div>
+         </div>
+         <br />
+         {/* JEE Mains Marks */}
+         <div  className="form-group row">
+            <label for="JEE"  className="col-sm-4 col-form-label">JEE Mains Marks: </label>
+            <div  className="col-sm-8">
+                <input type="jee-mains-marks" step="0.01" className="form-control" id="JEE" placeholder="93.26"  value={jeeMainsMarks} onChange={(event) => setJeeMainsMarks(event.target.value)} />
+            </div>
+         </div>
+         <br />
+          <input type="submit" value="Submit" className="btn btn-primary" />
+        </form>
+      </div>
+    </div>
   );
 }
 
