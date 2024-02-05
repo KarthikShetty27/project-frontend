9e3fce02131720b78f78918dabb8a7610ac4fe38
KarthikShetty27 <karthikshetty270902@gmail.com>
2024-02-05 23:01:26 +0530

    Changes-2.3.0

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,5 +1,8 @@
+// Form.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import Result from './Result';
+import { useNavigate } from 'react-router-dom';
 
 function Form() {
   const [age, setAge] = useState('');
@@ -8,6 +11,9 @@ function Form() {
   const [hscMarks, setHscMarks] = useState('');
   const [mhtcetMarks, setMhtcetMarks] = useState('');
   const [jeeMainsMarks, setJeeMainsMarks] = useState('');
+  const [result, setResult] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  const navigate = useNavigate(); 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -22,81 +28,99 @@ function Form() {
     };
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/form-data', formData);
-      
+      // Submit Form Data
+      const response = await axios.post('http://127.0.0.1:5000/submit-form', formData);
       console.log('Form submitted successfully!');
       console.log('Response:', response.data);
+
+      // Make prediction request
+      const predictResponse = await axios.post('http://127.0.0.1:5000/predict', formData);
+      console.log('Prediction Response:', predictResponse.data);
+
+      // Set the prediction result in the state
+      setResult(predictResponse.data);
+
+      // Update state to indicate form submission
+      setSubmitted(true);
+
+      // Redirect to the result page
+      navigate('/form-result');
     } catch (error) {
       console.error('Error during form submission:', error);
     }
   };
 
-  return (
-    <div id="finput">
-      <h2> Form Details </h2>
-      <div className='form-data'>
-        <form onSubmit={handleSubmit}>
-          {/* Age */}
-          <div className="form-group row">
-            <label for="Age"  className="col-sm-4 col-form-label">Age: </label>
-            <div className="col-sm-8">
-              <input className="form-control" id="Age" type="text" placeholder="19-26" value={age} onChange={(event) => setAge(event.target.value)} required />
+  if (!submitted){
+    return (
+      <div id="finput">
+        <h2> Form Details </h2>
+        <div className='form-data'>
+          <form onSubmit={handleSubmit}>
+            {/* Age */}
+            <div className="form-group row">
+              <label htmlFor="Age"  className="col-sm-4 col-form-label">Age: </label>
+              <div className="col-sm-8">
+                <input className="form-control" id="Age" type="text" placeholder="19-26" value={age} onChange={(event) => setAge(event.target.value)} required />
+              </div>
             </div>
-          </div>
-          <br />
-          {/* Socioeconomic Background */}
-          <div  className="form-group row">
-            <label for="SEB"  className="col-sm-5 col-form-label">Socioeconomic Background: </label>
-            <div  className="col-sm-7">
-              <select id="inputState"  className="form-control" value={seb} onChange={(event) => setSeb(event.target.value)} required >
-                <option value="">--Please select an option--</option>
-                <option value="Open">Open</option>
-                <option value="SQ - State Quota">SQ - State Quota</option>
-                <option value="SC - Scheduled Castes">SC - Scheduled Castes</option>
-                <option value="ST - Scheduled Tribes">ST - Scheduled Tribes</option>
-                <option value="EWS - Economically Weaker Sections"> EWS - Economically Weaker Sections </option>
-                <option value="OBC - Other Backward Classes"> OBC - Other Backward Classes </option>
-              </select>
+            <br />
+            {/* Socioeconomic Background */}
+            <div  className="form-group row">
+              <label htmlFor="SEB"  className="col-sm-5 col-form-label">Socioeconomic Background: </label>
+              <div  className="col-sm-7">
+                <select id="inputState"  className="form-control" value={seb} onChange={(event) => setSeb(event.target.value)} required >
+                  <option value="">--Please select an option--</option>
+                  <option value="Open">Open</option>
+                  <option value="SQ - State Quota">SQ - State Quota</option>
+                  <option value="SC - Scheduled Castes">SC - Scheduled Castes</option>
+                  <option value="ST - Scheduled Tribes">ST - Scheduled Tribes</option>
+                  <option value="EWS - Economically Weaker Sections"> EWS - Economically Weaker Sections </option>
+                  <option value="OBC - Other Backward Classes"> OBC - Other Backward Classes </option>
+                </select>
+              </div>
             </div>
-          </div>
-          <br />
-          {/* SSC Marks */}
-          <div  className="form-group row">
-            <label for="SSC"  className="col-sm-4 col-form-label">SSC Marks: </label>
-            <div  className="col-sm-8">
-              <input type="ssc-marks" step="0.01" className="form-control" id="SSC" placeholder="85.56" value={sscMarks} onChange={(event) => setSscMarks(event.target.value)} required />
+            <br />
+            {/* SSC Marks */}
+            <div  className="form-group row">
+              <label htmlFor="SSC"  className="col-sm-4 col-form-label">SSC Marks: </label>
+              <div  className="col-sm-8">
+                <input type="ssc-marks" step="0.01" className="form-control" id="SSC" placeholder="85.56" value={sscMarks} onChange={(event) => setSscMarks(event.target.value)} required />
+              </div>
             </div>
-          </div>
-          <br />
-          {/* HSC Marks */}
-          <div  className="form-group row">
-            <label for="HSC"  className="col-sm-4 col-form-label">HSC Marks: </label>
-            <div  className="col-sm-8">
-                <input type="hsc-marks" step="0.01" className="form-control" id="HSC" placeholder="88.64"  value={hscMarks} onChange={(event) => setHscMarks(event.target.value)} required />
+            <br />
+            {/* HSC Marks */}
+            <div  className="form-group row">
+              <label htmlFor="HSC"  className="col-sm-4 col-form-label">HSC Marks: </label>
+              <div  className="col-sm-8">
+                  <input type="hsc-marks" step="0.01" className="form-control" id="HSC" placeholder="88.64"  value={hscMarks} onChange={(event) => setHscMarks(event.target.value)} required />
+              </div>
             </div>
-          </div>
-          <br />
-          {/* MHTCET Marks */}
-          <div  className="form-group row">
-            <label for="MHTCET"  className="col-sm-4 col-form-label">MHTCET Marks: </label>
-            <div  className="col-sm-8">
-                <input type="mhtcet-marks" step="0.01" className="form-control" id="MHTCET" placeholder="90.85" value={mhtcetMarks} onChange={(event) => setMhtcetMarks(event.target.value)} required />
+            <br />
+            {/* MHTCET Marks */}
+            <div  className="form-group row">
+              <label htmlFor="MHTCET"  className="col-sm-4 col-form-label">MHTCET Marks: </label>
+              <div  className="col-sm-8">
+                  <input type="mhtcet-marks" step="0.01" className="form-control" id="MHTCET" placeholder="90.85" value={mhtcetMarks} onChange={(event) => setMhtcetMarks(event.target.value)} required />
+              </div>
             </div>
-          </div>
-          <br />
-          {/* JEE Mains Marks */}
-          <div  className="form-group row">
-            <label for="JEE"  className="col-sm-4 col-form-label">JEE Mains Marks: </label>
-            <div  className="col-sm-8">
-                <input type="jee-mains-marks" step="0.01" className="form-control" id="JEE" placeholder="93.26"  value={jeeMainsMarks} onChange={(event) => setJeeMainsMarks(event.target.value)} />
+            <br />
+            {/* JEE Mains Marks */}
+            <div  className="form-group row">
+              <label htmlFor="JEE"  className="col-sm-4 col-form-label">JEE Mains Marks: </label>
+              <div  className="col-sm-8">
+                  <input type="jee-mains-marks" step="0.01" className="form-control" id="JEE" placeholder="93.26"  value={jeeMainsMarks} onChange={(event) => setJeeMainsMarks(event.target.value)} />
+              </div>
             </div>
-          </div>
-          <br />
-          <input type="submit" value="Submit" className="btn btn-primary" />
-        </form>
+            <br />
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </form>
+        </div>
       </div>
-    </div>
   );
+  } else {
+    // Render the Result component
+    return <Result result={result} />;
+  }
 }
 
 export default Form;
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,12 +1,17 @@
 // Result.js
-
+import React from 'react';
 import './assets/css/styles.css'; // Import the CSS from styles.css file
 
-const Result = ({ resultMessage }) => {
+const Result = ({ result }) => {
+  if (!result) {
+    return <div> null </div>; // Render nothing if the result is not available yet
+  }
+
   return (
     <div id="foutput">
-      <h2>Form Pred-Result</h2>
-      <p>{resultMessage}</p>
+      <h2>Form Prediction Result</h2>
+      <p>College Predictions: {result.collegePredictions}</p>
+      <p>Branch Predictions: {result.branchPredictions}</p>
     </div>
   );
 };
